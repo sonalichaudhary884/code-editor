c35fe99d722a22a3962c06b1d62df0f0f8ceabd1
sonalichaudhary844@gmail.com <sonalichaudhary884>
2025-01-22 17:38:10 +0530

    codepen done

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
@@ -6,18 +6,28 @@ import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import { Controlled as CodeMirror } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Editor(props) {
   const { language, displayName, onChange, value } = props;
+
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button
+          type="button"
+          className="expand-collapes-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <CodeMirror
         onBeforeChange={handleChange}
